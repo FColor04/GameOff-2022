3c487347c129ed091a3eb09016188837ca5232bb
Umbrason <45980080+Umbrason@users.noreply.github.com>
2022-11-12 01:59:01 +0100

    refactor playerMovementModificators

--- Assets/Scripts/Characters/PlayerMovement.cs
+++ Assets/Scripts/Characters/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(MovementController), typeof(PlayerCamera))]
@@ -10,16 +11,16 @@ public class PlayerMovement : MonoBehaviour
 
     private MovementController cached_movementController;
     private MovementController MovementController { get => cached_movementController ??= GetComponent<MovementController>(); }
-    
+
     private PlayerEquipment cached_playerEquipment;
     private PlayerEquipment PlayerEquipment { get => cached_playerEquipment ??= GetComponent<PlayerEquipment>(); }
-    
+
     public Vector2 MovementInput { get; set; }
     [field: SerializeField] public float Speed { get; set; } = 7f;
-    [field: SerializeField] public float DownsightsSpeed { get; set; } = 5f;
+    public readonly Dictionary<string, float> MovementSpeedModifiers = new();
 
     void FixedUpdate()
     {
-        MovementController.XZPlaneMovement = Quaternion.Euler(0, 0, -PlayerCamera.RY) * MovementInput * (PlayerEquipment.aimingDownsights ? DownsightsSpeed : Speed);
+        MovementController.XZPlaneMovement = Quaternion.Euler(0, 0, -PlayerCamera.RY) * MovementInput * Speed * MovementSpeedModifiers.Values.Aggregate((x, y) => x * y);
     }
 }
